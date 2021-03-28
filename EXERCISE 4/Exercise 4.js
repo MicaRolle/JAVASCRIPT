89c88912ae752391af777c83220cd47f994ebbdd
Mica Rolle <micarolle_47@hotmail.com>
2021-03-28 20:44:55 -0300

    correccion ejercicio 4.b

--- EXERCISE 4/Exercise 4.js
+++ EXERCISE 4/Exercise 4.js
@@ -11,19 +11,21 @@ var varone= Math.random();
 
 //* b //*
 
+var 9ni¡
+
 var age = Math.round((Math.random()*100));
 
     if (age <= 2){
         alert('Bebe');
-    }else if (age > 3 && age < 12){
+    }else if (age > 3 && age <= 12){
             alert('Niño');
-    }else if (age > 13 && age < 19){
+    }else if (age > 13 && age <= 19){
             alert('Adolecente');
-    }else if (age > 20 && age < 30){
+    }else if (age > 20 && age <= 30){
             alert('Joven');
-    }else if (age > 31 && age < 60) {
+    }else if (age > 31 && age <= 60) {
            alert('Adulto')
-    }else if (age > 61 && age < 75) {
+    }else if (age > 61 && age <= 75) {
             alert('Adulto Mayor')
     }else {
         alert('Anciano')
